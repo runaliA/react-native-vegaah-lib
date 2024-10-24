8134417dc24d0ac3c552ddbc7dcd6353dc386bce
runali1911 <runali.ambavkar@concertosoft.com>
2024-10-24 18:37:38 +0530

    Initial Commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 
+import {
+  StyleSheet,
+  Text
+} from 'react-native';
+
+//import WebView from 'react-native-webview';
 
 
-
-    export const v2plugin = ( props: {
+    export const hostedplugin = ( props: {
       data: string | null | undefined   }) =>
    {
     const varcurrency = 'SAR';
@@ -13,4 +18,25 @@
     console.log(varcurrency);
     console.log('SA');
     console.log(amount);
+
+  
+
+    return (
+      <Text
+       
+        style={styles.webview}
+      >
+        In Plugin Data
+        </Text>
+    );
 }
+const styles = StyleSheet.create({
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  webview: {
+    flex: 1,
+  },
+});
