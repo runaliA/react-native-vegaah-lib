74c3c4d79301e5b29cd162bc6d5549260628e704
runali1911 <runali.ambavkar@concertosoft.com>
2024-10-28 15:43:02 +0530

    Componentt

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,21 @@
 
 import {
   StyleSheet,
-  View
+  View,  SafeAreaView
 } from 'react-native';
 // import CustomWebView from './CustomWebView';
 import WebView from 'react-native-webview';
 
 
 
-    export const HostedPlugin   = ( props: {
-      data: string | null | undefined   }) =>
-   {
+    export const HostedPlugin  : React.FC<{ data: string | null | undefined }> = ({ data }) => {
+       
+ 
     const varcurrency = 'SAR';
     const amount = '1';
-    const url = "https://www.google.com";
-    const reqparams:any  = (props.data);
+    //const url = "https://www.google.com";
+    console.log("DATA  "+ data)
+    const reqparams:any  = (data);
     const requestdata :any =JSON.parse(reqparams);
     console.log("In ELSE of Apple pay "+ JSON.stringify(requestdata));
     console.log(varcurrency);
@@ -23,19 +24,36 @@ import WebView from 'react-native-webview';
 
     return (
      
-        <View style={styles.loadingContainer}>
-          <WebView source={{ uri: url }} style={styles.webview} />
-        </View>
+      <SafeAreaView style={styles.container}>
+      <View style={styles.btnContainer}>
+      <WebView
+            source={{ uri: "https://blog.logrocket.com/" }}
+            style={{ flex: 1 }}
+            onLoadStart={() => console.log('WebView load started')}
+            onError={(syntheticEvent) => {
+              const { nativeEvent } = syntheticEvent;
+              console.error('WebView error: ', nativeEvent);
+            }}
+          />
+          </View>
+    </SafeAreaView>
   
     );
 }
 const styles = StyleSheet.create({
-  loadingContainer: {
+  container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    
   },
-  webview: {
+  webViewContainer: {
     flex: 1,
+    width: '100%',
+    height: '100%',
   },
+  btnContainer:
+   {
+    flex : 1,
+    justifyContent : 'center',
+    alignContent : 'center'
+   }
 });
