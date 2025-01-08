251e8713f1979764c75df185a1c6bb9e1bf71713
runali1911 <runali.ambavkar@concertosoft.com>
2025-01-08 17:29:32 +0530

    URL View Changes

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,7 @@ import {
 import WebView from 'react-native-webview';
 import React, { useEffect, useState } from 'react';
 import CryptoJS from 'crypto-js';
+import DeviceInfo from 'react-native-device-info';
 
 
 interface HostedPluginProps {
@@ -24,29 +25,34 @@ const HostedPlugin: React.FC<HostedPluginProps>   = ({ data, onClose }) => {
   //const [currentUrl, setCurrentUrl] = useState("");
  // const [reqHash , setreqHash ] = useState("")
   //console.log("DATA " + data);
+  const reqparams: any = data;
+  const requestdata: any = JSON.parse(reqparams);
   useEffect(() => {
     const fetchPayment = async () => {
       //const varcurrency = 'SAR';
       //const amount = '1';
       console.log("DATA " + data);
-      const reqparams: any = data;
-      const requestdata: any = JSON.parse(reqparams);
+     
       console.log("In ELSE of Apple pay " + JSON.stringify(requestdata));
 
       const txn_details = "" + requestdata.trackid + "|" + requestdata.terminalId + "|" + requestdata.password + "|" + requestdata.merchantkey + "|" + requestdata.amount +
       "|" + requestdata.currency + "";
       const hash = CryptoJS.SHA256(txn_details).toString(CryptoJS.enc.Hex);
     console.log('SHA-256 Hash:', hash);
-   // setreqHash(hash);
-  //     JSHash(txn_details, CONSTANTS.HashAlgorithms.sha256)
-  // .then((hash) => 
-  //   {
-  //     console.log(hash)
-  //     setreqHash(hash);
+    let devicejson ={};
+    let appName = DeviceInfo.getSystemName();
+    console.log("appName : "+appName);
+      devicejson = {
+                             'pluginName': "React Native ",
+                             'pluginVersion': '1.0.0',
+                             'pluginPlatform': DeviceInfo.getDeviceType(),
+                             'deviceModel': DeviceInfo.getModel(),
+                             'devicePlatform': DeviceInfo.getSystemName(),
+                             'deviceOSVersion': DeviceInfo.getSystemVersion(),
+                   };
 
-  //   })
-  // .catch(e => console.log(e));
   console.log("signature", hash);
+  const json_devicedata = JSON.stringify(devicejson);
       const paymentRequest = {
         terminalId: requestdata.terminalId,
         paymentType: requestdata.action,
@@ -64,13 +70,26 @@ const HostedPlugin: React.FC<HostedPluginProps>   = ({ data, onClose }) => {
       },
         currency: requestdata.currency,
         customerIp: '10.10.10.10',
+        referenceId : requestdata.transactionId,
         applepayId: 'applepay',
         order: {
           orderId: requestdata.trackid,
           description: "Purchase of product XYZ"
-        }
+        },
+        country:requestdata.country,
+        tokenization: {
+        "cardToken": "",
+        "operation": requestdata.tokenizationType
+    },
+    additionalDetails: {
+      'userData': requestdata.metadata
+      
+    },
+    'deviceInfo' : json_devicedata
       };
-let requrl = "https://test-vegaah.concertosoft.com/vegaahpayments/v2/payments/pay-request"
+
+      console.log("  REQUEST "+JSON.stringify(paymentRequest))
+let requrl = requestdata.requestUrl
       try {
         const response = await fetch(requrl, {
           method: 'POST',
@@ -116,7 +135,7 @@ useEffect(() => {
       if(showWebView)
       {
     // Send the result back to the parent app
-    onClose(paymentResult);
+    onClose(JSON.stringify(paymentResult));
     // Hide WebView after sending result
     setShowWebView(false);
       }
@@ -189,7 +208,7 @@ useEffect(() => {
 
             // let data123 =
             // 'StWTroAjr/0PZYMM1bDoFn60i833Xws27bNws++fjZbbw+Yf/7xpF8HqA3t1BxqOoOD/HSYTvpxqrE59NPE1fB6E1lNo8GTaLX4x+u2YVrcH7VeQ0/jObUtAKI67YVXikCcp64K+Ys+EllpYuha8/nMeLEbGjWDLT+a7jVIBr9SRFZkwUNGi8US3dRKLi9SK5oSuIRw4axokM+XiEcQx0cVrDM41HBcDR/5hX61mWTtcr5tmrHLl7UMptcfhUZwFf/5Z/G8iB7Ju5d5oN35vMc0Lf/3GZxTHpx5vMqRWfRyx7dYO21UXneIjwH41l8xEcTDEOPIGnvXmpC+V13IoqbZH/PNl5CEMtRsX7Mmri+XaMKw6Ic3YiJPsme9BRHkfyb46Qu151rDwbYfcuSwvpKxEX/XTJ/6cWc1nid+AgqgKCdiC1viRZKhOHef7+nH0NBkleAMX8eOdBhJc2JYTHoS9mxkinLsewPtyNuiYVUkItZa8B6ZXh1FTMLVnUe/Rw28uEbuLsIGm6i5v7l4THQBa/7j+AZGuz9PIH68/uPIGFsKOLKQeY4jwiYNknw+YH8aiIUvsbxt3Q30LFJseOmAU9zTjHHVjFW//pIVTkwyfenpa8uNyuoUwNGBQlcrBoxNGHSLPYR/pGmz3qtvAxF/ZCdz1xEHIFEownpt1YXvbS/ROfCP4E5ZO5h57PB05pfAXNnGGW8GguBtXiiywMIUMZ0nV32H2EtbQnGN93k7kJEI9L1v61h+24V/XrPdmzSzu3VlHPStgY/2H0vM6/rSRPHdX9C65+0tNzOfVaLOVbC7UJwWVVVB8Oo06r5ti1Cm9nZzWwiFqT80c75Lh0I05BhyrEHCPyslMndaMBBGgd/N+DtTTDrjK+qmRRFbs';
-             const secretKey = "3b8babda62a62fecce0d3152868f2d3aced314033692da4346968d1b18eb7998";
+             const secretKey = requestdata.merchantkey;
               console.log("Merchant Data "+decodedString)
               const result = decryptAES256ECB(decodedString, secretKey);
               console.log("FINAL RESPONSE " + result);
